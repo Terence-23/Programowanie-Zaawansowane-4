1d38afa036e0566ac5190a15fa47f2f59ba069bf
terence-23 <jpuchalski1703@gmail.com>
2024-09-06 12:17:12 +0200

    add some basics

--- server/app.py
+++ server/app.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask import render_template
+
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello():
-    return '<p>Hello, World!</p>'
+    return render_template('index.html')
+
+
+@app.route('/profile')
+def profile():
+    return render_template("profile.html")
+@app.route('/info')
+def info():
+    return render_template("info.html")
+
